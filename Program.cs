a677ddbc0662f0455dbcc59f72a6bdfff26f9651
Frutios <benjamin.tourneur7@gmail.com>
2021-02-27 21:31:00 +0100

    quest resolved

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Entiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.MaxValue;
+            long y = long.MaxValue;
+            short z = short.MaxValue;
+
+            ulong w = (ulong)(x + y + z);
+            Console.WriteLine(w);
+        }
+    }
+}
